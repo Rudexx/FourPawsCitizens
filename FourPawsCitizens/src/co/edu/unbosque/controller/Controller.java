b44156b9cf485761c9e74f148edc6abbbb744009
unknown <danielalvarez202@gmail.com>
2021-02-25 20:29:58 -0500

    Controller mejorado

--- FourPawsCitizens/src/co/edu/unbosque/controller/Controller.java
+++ FourPawsCitizens/src/co/edu/unbosque/controller/Controller.java
@@ -1,9 +1,36 @@
 package co.edu.unbosque.controller;
 
+import java.util.Scanner;
+
 public class Controller {
 	
 	public Controller() {
-		System.out.println("Perrito y Gatitos");
+		System.out.println("Perritos y Gatitos");
+		System.out.println("Seleccione la operacion a realizar:" + "\n1) Cargar Archivo CSV" + "\n2) Asignar id a los animales" 
+		                   + "\n3) Buscar Animal por microchip" + "\n4) Contar animales por especie" + "\n5) Buscar Animales"
+				           + "\n6) Buscar id");
+		Scanner leer = new Scanner(System.in);
+		String opcion = leer.nextLine();
+		switch(opcion){
+		case "1":
+			
+			break;
+		case "2":
+			
+			break;
+		case "3":
+			
+			break;
+		case "4":
+			
+			break;
+		case "5":
+			
+			break;
+		case "6":
+			
+			break;
+		}
 	}
 	
 }
